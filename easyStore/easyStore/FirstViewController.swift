8d69eaa9ff110e19ec5b0186a70b6f19597ad640
黄玮晟 <huangweisheng@huangweishengdeMacBook-Air.local>
2017-03-15 15:21:59 +0800

    create

---
+++ easyStore/easyStore/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  easyStore
+//
+//  Created by 黄玮晟 on 2017/3/15.
+//  Copyright © 2017年 黄玮晟. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
